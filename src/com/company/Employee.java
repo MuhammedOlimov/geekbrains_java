2ef27cb25c251e104bcd95a33d4abeb9253cae24
Muhammed <24434muhammed@gmail.com>
2020-05-24 19:22:12 +0500

    lesson5 have done

---
+++ src/com/company/Employee.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void  getFullInfo() {
+        System.out.println(String.format(" fullName: %s%n position: %s%n email: %s%n phone: %s%n salary: %s%n age: %s%n", fullName, position, email, phoneNumber, salary, age));
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
