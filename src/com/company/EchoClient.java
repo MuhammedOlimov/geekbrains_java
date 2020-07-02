0e8c7ad59282c0c6bdaf9bc56b7247b41c30f753
Muhammed <24434muhammed@gmail.com>
2020-07-02 16:38:51 +0500

    сделано можете проверить)

--- src/com/company/EchoClient.java
+++ src/com/company/EchoClient.java
@@ -9,137 +9,45 @@ import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class EchoClient extends JFrame {
-    private final String SERVER_ADDR = "localhost";
-    private final int SERVER_PORT = 8189;
-
-    private JTextField msgInputField;
-    private JTextArea chatArea;
-
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
-
-    public EchoClient() {
-        try {
-            openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        prepareGUI();
+public class EchoClient {
+    public static void main(String[] args) {
+        EchoClient  echoClient = new EchoClient();
     }
+    public EchoClient(){
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 8189);
+            Scanner in =  new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
 
-    public void openConnection() throws IOException {
-        socket = new Socket(SERVER_ADDR, SERVER_PORT);
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
                     while (true) {
-                        String strFromServer = in.readUTF();
-                        if (strFromServer.equalsIgnoreCase("/end")) {
-                            break;
-                        }
-                        chatArea.append(strFromServer);
-                        chatArea.append("\n");
+                        String msg = in.nextLine();
+                        System.out.println(msg);
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
+            }).start();
+
+            while (true) {
+                String msg = sc.nextLine();
+                out.println("Client: " + msg);
             }
-        }).start();
-    }
-
-    public void closeConnection() {
-        try {
-            in.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        try {
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void sendMessage() {
-        if (!msgInputField.getText().trim().isEmpty()) {
+        } finally {
             try {
-                out.writeUTF(msgInputField.getText());
-                msgInputField.setText("");
-                msgInputField.grabFocus();
+                socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
             }
         }
     }
-
-    public void prepareGUI() {
-        // Параметры окна
-        setBounds(600, 300, 500, 500);
-        setTitle("Клиент");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        // Текстовое поле для вывода сообщений
-        chatArea = new JTextArea();
-        chatArea.setEditable(false);
-        chatArea.setLineWrap(true);
-        add(new JScrollPane(chatArea), BorderLayout.CENTER);
-
-        // Нижняя панель с полем для ввода сообщений и кнопкой отправки сообщений
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        JButton btnSendMsg = new JButton("Отправить");
-        bottomPanel.add(btnSendMsg, BorderLayout.EAST);
-        msgInputField = new JTextField();
-        add(bottomPanel, BorderLayout.SOUTH);
-        bottomPanel.add(msgInputField, BorderLayout.CENTER);
-        btnSendMsg.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
-        msgInputField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
-
-        // Настраиваем действие на закрытие окна
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-                try {
-                    out.writeUTF("/end");
-                    closeConnection();
-                } catch (IOException exc) {
-                    exc.printStackTrace();
-                }
-            }
-        });
-
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new EchoClient();
-            }
-        });
-    }
 }
+
--- src/com/company/EchoServer.java
+++ src/com/company/EchoServer.java
@@ -3,27 +3,43 @@ package com.company;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
     public static void main(String[] args) {
+        EchoServer echoServer = new EchoServer();
+     }
+    public EchoServer () {
         Socket socket = null;
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner in = new Scanner(socket.getInputStream());
+            Scanner sc = new Scanner(System.in);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+                        String msg = sc.nextLine();
+                        out.println("Server: " + msg);
+                    }
+                }
+            }).start();
             while (true) {
-                String str = in.readUTF();
+                String str = in.nextLine();
                 if (str.equals("/end")) {
                     break;
                 }
-                out.writeUTF("Эхо: " + str);
+                System.out.println(str);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
